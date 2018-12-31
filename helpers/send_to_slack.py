fac3cacef97dbea686bba700d264245e307119f5
Timo Cornelius Metzger <>
2018-12-31 20:33:58 +0100

    :sparkles: Send image to slack, if one is present

--- helpers/send_to_slack.py
+++ helpers/send_to_slack.py
@@ -8,9 +8,43 @@ SLACK_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 CHANNEL = os.environ.get('SLACK_CHANNEL')
 CLIENT = SlackClient(SLACK_TOKEN)
 
-def message_to_slack(message, channel=CHANNEL):
-    CLIENT.api_call(
-        'chat.postMessage',
-        channel=channel,
-        text=message,
-    )
+def create_attachment(story):
+    """
+    Create attachment info for slack
+    Return attachment
+    """
+    #print(f'++++++++++++++++++++++++++++++++slack function: {story}')
+    attached = [
+        {
+            'fallback': f'Vorschaubild',
+            'color': '#162486',
+            'title': story['media']['image'][0]['caption'],
+            'text': story['title'],
+            'image_url': story['media']['image'][0]['url'],
+            'callback_id': story['id'],
+            'footer': 'Vorschaubild:',
+        }
+    ]
+    print(attached)
+    return attached
+
+def message_to_slack(message, attached='none', channel=CHANNEL):
+    """
+    Send message (with attachment, if provided), to slack channel
+    Return send status
+    """
+    if not attached == 'none':
+        print('with attachment')
+        return CLIENT.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text=message,
+            attachments = attached,
+        )
+    else:
+        print('NOT with attachment')
+        return CLIENT.api_call(
+            'chat.postMessage',
+            channel=channel,
+            text=message,
+        )
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 from config import STATE_FILTER, DELAY, DOWNLOAD_LIMIT
 from helpers.log_to_logfile import add_to_log
-from helpers.send_to_slack import message_to_slack
+from helpers.send_to_slack import message_to_slack, create_attachment
 import db
 import regex_filters
 
@@ -22,7 +22,13 @@ if __name__ == '__main__':
                 filter_result = regex_filters.check_filter(item)
                 if filter_result:
                     print(f'+++ {" ".join(str(s) for s in filter_result)} +++')
-                    message_to_slack(f'{datetime_published.strftime("%d.%m.%Y %H:%M:%S")} {story_title} ({story["url"]}) [{" ".join(str(s) for s in filter_result)}]')
+                    output_message = f'{datetime_published.strftime("%d.%m.%Y %H:%M:%S")} {story_title} ({story["url"]}) [{" ".join(str(s) for s in filter_result)}]'
+                    if 'Image' in str(filter_result): # Create attachment for slack for items with image 
+                        attached = create_attachment(story)
+                        message_to_slack(output_message, attached)
+                    else: # Send straight to slack if no image present
+                        message_to_slack(output_message)
+                    # message_to_slack(f'{datetime_published.strftime("%d.%m.%Y %H:%M:%S")} {story_title} ({story["url"]}) [{" ".join(str(s) for s in filter_result)}]')
                     add_to_log(f'{" ".join(str(s) for s in filter_result)}, {story["url"]}', 'info')
         else:
             print('No new stories found.')
